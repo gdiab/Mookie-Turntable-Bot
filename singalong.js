174f70b3f469f2991177396daabdf9ab5745a34a
George Diab <me@georgediab.com>
2012-02-28 23:22:17 -0800

    Updated files to match last mookie build

--- singalong.js
+++ singalong.js
@@ -18,7 +18,24 @@ var blastoff = [
 var sail = [
     ['SAIL', 34500]
 ];
+
+var atomic garden = [
+    ['And I\'m glad I\'m not Gorbachev', 69000],
+    ['\'cause I\'d wiggle all night', 69500],
+    ['Like jelly in a pot', 72000],
+    ['Let\'s get in the pit!', 74500], 
+    ['And a party that will never stop', 76500],
+    ['Come out to plaaaaaaaaay!', 78900]
+];
     
+var new dark ages = [
+    ['Can you hear the call!', 184000],
+    ['We\'ll set this riiiiight!', 186000],
+    ['I hope you\'re livin\' right!', 188000],
+    ['At leats he\'s got a garden with a fertile plot', 190000], 
+    ['These are the new dark ages!', 192000]
+];
+
 var songlist = [
     ['AWOLNATION', 'Sail', sail],
     ['Reptar', 'Blastoff', blastoff],  
